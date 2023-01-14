266be6756fc544bc32b774b2d6987c7518a5ab8e
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2023-01-14 21:50:26 +0530

    Optimal solution of Insertion sort

--- Insertion sort.cpp
+++ Insertion sort.cpp
@@ -1,4 +1,46 @@
 /* Codes for Insertion sort
 Problem statement - https://www.codingninjas.com/codestudio/problems/insertion-sort_3155179 */
+
 // Solution - 1 
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+ for(int i=0;i<n-1;i++){
+     if(arr[i+1]<arr[i]){
+         swap(arr[i+1],arr[i]);
+         for(int j=i;j>0;j--){
+             if(arr[j]<arr[j-1]){
+                 swap(arr[j],arr[j-1]);
+             }
+         }
+     }
+ }
+}
+
+//OPTIMAL SOLUTION 
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+   // Write your code here.
+    int result;
+   int i = 1; 
+   while (i<n)
+   {
+        result = arr[i];
+       int j = i-1;
+       while (j>=0)
+       {
+           if (arr[j]> result)
+           {
+               arr[j+1] = arr[j];
+           }
+           else 
+           {
+               break;
+           }
+
+           j--;
+       }
+       arr[j+1]=result;
+       i++;
+   }
+}
 
