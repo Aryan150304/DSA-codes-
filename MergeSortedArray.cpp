b12f133f24d1b65fc63a446cfffea01fe5d2826c
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2023-01-16 22:07:23 +0530

    Added 2nd optimal solution

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -15,3 +15,37 @@ public:
 
 //OPTIMAL SOLUTION (3ms runtime and beats 74% solution)
 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        vector<int> ans ;
+        int j = 0;
+        while(i<(m+n)&& ans.size()<m+n){
+            if(i>=m&&j<n){
+                ans.push_back(nums2[j++]);
+            }
+            else if(j>=n){
+                ans.push_back(nums1[i++]);
+            }
+           else if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i++]);
+            }
+            else if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i++]);
+                ans.push_back(nums2[j++]);
+
+            }
+            else if(nums1[i]>nums2[j]){
+                ans.push_back(nums2[j++]);
+             }
+            
+    }
+    if(ans.size()>0){
+    nums1 = ans ;
+    }
+    else if(m==0){
+      nums1=nums2;
+     }
+    }
+};
