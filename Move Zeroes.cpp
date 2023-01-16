17ea9b8af99064b0f4b35b411dbf2f26fbfba20e
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2023-01-16 22:09:14 +0530

    Move zeroes

--- Move Zeroes.cpp
+++ Move Zeroes.cpp
@@ -1,1 +1,17 @@
-
+/* This is a leetode problem where all the zeroes have to placed in end of the array such that the order of the array doesn't change 
+Problem link  - https://leetcode.com/problems/move-zeroes/description/
+SOLUTION - */
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+    int i = 0;
+    int j =0;
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    }
+};
