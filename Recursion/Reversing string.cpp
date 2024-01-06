7260f1bda5174dba0cad88eef7da6455c85b7d45
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-06 22:25:22 +0530

    Added coding ninjas solution too

--- Recursion/Reversing string.cpp
+++ Recursion/Reversing string.cpp
@@ -25,3 +25,27 @@ int main(){
     cout<<str<<endl;
     return 0;
 }
+
+
+/// adding coding ninjas solution too
+#include <bits/stdc++.h> 
+
+void reverse(string &str, int i , int j){
+    if(i>=j){
+        return ;
+    }
+    swap(str[i],str[j]);
+    i++;
+    j--;
+    reverse(str,i,j);
+}
+
+
+string reverseString(string str)
+{
+	int i =0;
+	int j = str.length()-1;
+	reverse(str,i,j);
+	return str;
+	
+}
