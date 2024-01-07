60e391db007b186fbf366774445b9fededf1e8bc
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-07 18:15:24 +0530

    Create Inversion Count problem.cpp

---
+++ Recursion/Inversion Count problem.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int countIndex =0;
+void merge(int *arr, int start, int end){
+    int mid = (start+end)/2;
+    int len1 = (mid-start)+1;
+    int len2 = end - mid ;
+    
+    int *arr1 = new int[len1];
+    int *arr2 = new int[len2];
+    
+    // setting the values
+    for(int i=0;i<len1;i++){
+        arr1[i]=arr[i+start];
+    }
+    
+     for(int i=0;i<len2;i++){
+         arr2[i] = arr[i+mid+1];
+    }
+    
+    // comparison 
+    int i =0;
+    int j = 0;
+    int mainIndex = start;
+    
+    while(i<len1&&j<len2){
+        if(arr1[i]<=arr2[j]){
+            arr[mainIndex++]=arr1[i++];
+        }
+        else{
+            arr[mainIndex++]=arr2[j++];
+            countIndex+=(len1-i);
+        }
+    }
+    while(i<len1){
+        arr[mainIndex++]=arr1[i++];
+    }
+     while(j<len2){
+        arr[mainIndex++]=arr2[j++];
+    }
+    
+    delete []arr1;
+    delete []arr2;
+}
+
+void mergeSort(int *arr, int start, int end){
+    if(start>=end){
+        return;
+    }
+    int mid = (start+end)/2;
+    mergeSort(arr,start,mid);
+    mergeSort(arr,mid+1,end);
+    merge(arr,start,end);
+}
+
+int main(){
+    int arr[5]={8, 4, 2, 1};
+    mergeSort(arr,0,3);
+    for(int i=0;i<4;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl<<countIndex<<endl;
+    
+}
