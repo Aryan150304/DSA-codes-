602da22cbcf3ecadc2ab7c28a2d450125645b6d4
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-12 23:37:15 +0530

    Update Determine if String Halves Are Alike.cpp

--- Strings/Determine if String Halves Are Alike.cpp
+++ Strings/Determine if String Halves Are Alike.cpp
@@ -28,3 +28,34 @@ public:
     }
 };
 
+
+// approach 2 
+
+lass Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int i = 0;
+        int counti = 0;
+        int j = s.length()-1;
+        int countj=0;
+        while(j>=i){
+            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'){
+                countj++;
+            }
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                counti++;
+                cout<<counti<<endl;
+            }
+            j--;
+            i++;
+        }
+        cout<<counti<<" "<<countj<<endl;
+        if(counti==countj){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
