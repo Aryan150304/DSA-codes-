f71282eef785bf40aa772ccb444a9586b080f7c0
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-12 23:34:25 +0530

    Update Determine if String Halves Are Alike.cpp

--- Strings/Determine if String Halves Are Alike.cpp
+++ Strings/Determine if String Halves Are Alike.cpp
@@ -1,3 +1,30 @@
 // leetcode daily problem - https://leetcode.com/problems/determine-if-string-halves-are-alike/description/?envType=daily-question&envId=2024-01-12
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int i = 0;
+        int counti = 0;
+        int j = s.length()-1;
+        int mid = (i+j)/2;
+        int countj=0;
 
+        for(int j=0;j<=mid;j++){
+            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'){
+                counti++;
+            }
+        }
+         for(int j=mid+1;j<=s.length()-1;j++){
+            if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'){
+                countj++;
+            }
+        }
+
+        if(counti==countj){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
 
