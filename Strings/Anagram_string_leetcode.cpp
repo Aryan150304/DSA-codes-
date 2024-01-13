a6e2aeef318ebf9a3d537bbf857c2573c20a6d37
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-13 22:53:30 +0530

    Create Anagram_string_leetcode.cpp

---
+++ Strings/Anagram_string_leetcode.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        vector<int> first(26,0);
+        vector<int> second(26,0);
+        for(int i=0;i<s.length();i++){
+            int elem = s[i] -'a';
+            first[elem]++;
+        }
+
+         for(int i=0;i<t.length();i++){
+            int elem = t[i] -'a';
+            second[elem]++;
+        }
+        int count =0;
+
+        for(int i=0;i<26;i++){
+            if((first[i]>second[i])){
+                count+=(first[i]-second[i]);
+                cout<<count;
+            }
+        }
+        return count;
+        
+    }
+};
