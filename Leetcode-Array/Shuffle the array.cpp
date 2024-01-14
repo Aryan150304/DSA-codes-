96c4c540ad4bfd90ba339c03d142baa98367b353
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-14 23:31:01 +0530

    Update Shuffle the array.cpp

--- Leetcode-Array/Shuffle the array.cpp
+++ Leetcode-Array/Shuffle the array.cpp
@@ -1,2 +1,24 @@
 // leetcode link - https://leetcode.com/problems/shuffle-the-array/submissions/1146200586/
 
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> firstPart;
+        vector<int> secondPart;
+        for(int i=0;i<n;i++){
+            firstPart.push_back(nums[i]);
+        }
+        for(int i=n;i<nums.size();i++){
+            secondPart.push_back(nums[i]);
+        }
+
+        int i =0;
+        int j =0;
+        int index = 0;
+        while(i<firstPart.size()||j<secondPart.size()){
+            nums[index++] = firstPart[i++];
+            nums[index++] = secondPart[j++];
+        }
+        return nums;
+    }
+};
