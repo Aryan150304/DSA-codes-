9c6fb9f337f4a9eab693a24968a5d09db9e9bd21
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-14 23:38:49 +0530

    Update Shuffle the array.cpp

--- Leetcode-Array/Shuffle the array.cpp
+++ Leetcode-Array/Shuffle the array.cpp
@@ -22,3 +22,11 @@ public:
         return nums;
     }
 };
+
+// approach 2 
+ vector<int> ans;
+       for(int i=0;i<n;i++){
+           ans.push_back(nums[i]);
+           ans.push_back(nums[n+i]);
+       }
+       return ans;
