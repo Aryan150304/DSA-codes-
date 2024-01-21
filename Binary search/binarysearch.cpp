d28fdf744f90103b561dc9d8ab2d82b4437ed143
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-21 17:43:34 +0530

    Update binarysearch.cpp

--- Binary search/binarysearch.cpp
+++ Binary search/binarysearch.cpp
@@ -1,1 +1,23 @@
+// leetcode link - https://leetcode.com/problems/binary-search/description/
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = (start + end) / 2;
+        while (start <= end) {
+            if (nums[mid] == target) {
+                ans = mid;
+                break;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            mid = (start + end) / 2;
+        }
+        return ans;
+    }
+};
