44806159fb730733af7faa4bb9b2a125b14b80fd
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-22 21:52:33 +0530

    Update Set mismatch.cpp

--- Leetcode-Array/Set mismatch.cpp
+++ Leetcode-Array/Set mismatch.cpp
@@ -1,5 +1,8 @@
 // leetcode link - https://leetcode.com/problems/set-mismatch/?envType=daily-question&envId=2024-01-22
 
+// approach 1 to solve by calculating expected and actual sum 
+// approach 2 can be to store the frequency 
+// approach which is the most optimal and has o(1) space is finding XOR
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& arr) {
