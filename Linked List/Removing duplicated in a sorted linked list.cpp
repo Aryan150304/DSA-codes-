59b42cdc4ba39f332b9513d5c3dd524a58a49b4d
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-25 17:59:17 +0530

    Update Removing duplicated in a sorted linked list.cpp

--- Linked List/Removing duplicated in a sorted linked list.cpp
+++ Linked List/Removing duplicated in a sorted linked list.cpp
@@ -25,3 +25,49 @@ public:
         
     }
 };
+
+// follow up - to create a linked list which only contains element that comes once. If the element comes more than once is not included
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+        ListNode* forw = curr->next;
+        bool issame = false;
+        while (curr != NULL && curr->next != NULL) {
+            if (curr->val == curr->next->val) {
+                ListNode* forward = curr->next->next;
+                ListNode* todelete = curr->next;
+                delete todelete;
+                curr->next = forward;
+                issame = true;
+            } else if (issame == true) {
+                prev->next = curr->next;
+                ListNode* tobedeleted = curr;
+                curr = curr->next;
+                delete tobedeleted;
+                issame = false;
+            } else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        if (issame == true) {
+            prev->next = curr->next;
+            ListNode* tobedeleted = curr;
+                curr = curr->next;
+                delete tobedeleted;
+        }
+
+        // delete dummy;
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
