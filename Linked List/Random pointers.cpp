13482a94489e4c986c2220486a818e78d6920458
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-01-27 21:48:58 +0530

    Update Random pointers.cpp

--- Linked List/Random pointers.cpp
+++ Linked List/Random pointers.cpp
@@ -1,19 +1,6 @@
 // approach 1 of creating a map between original and clone linked list and finding random pointer from original list and connecting to clone linked list 
 /*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
+
 
 class Solution {
 public:
@@ -60,5 +47,92 @@ public:
         }
         return temp2;
         
+    }
+};
+
+// approach 2 - changing the links and optimising the space complexity to O(n)
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void insertattail(Node*& head, Node*& tail, int data) {
+
+        Node* newnode = new Node(data);
+        if (head == NULL && tail == NULL) {
+            head = newnode;
+            tail = newnode;
+            return;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+    Node* copyRandomList(Node* head) {
+        // 1st method is copying the list creating a map
+        Node* clonehead = NULL;
+        Node* clonetail = NULL;
+        Node* temp = head;
+        while (temp != NULL) {
+            insertattail(clonehead, clonetail, temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        Node* cloneNode = clonehead;
+        Node* originalNode = head;
+        // create the architecture using 6steps
+        while (originalNode != NULL) {
+            Node* next = originalNode->next;
+            originalNode->next = cloneNode;
+            originalNode = next;
+
+            Node* next2 = cloneNode->next;
+            cloneNode->next = originalNode;
+            cloneNode = next2;
+        }
+        // assign random nodes using that architecture
+        originalNode = head;
+        cloneNode = clonehead;
+        while (originalNode != NULL) {
+            if (originalNode->next != NULL) {
+                if (originalNode->random != NULL) {
+                    originalNode->next->random = originalNode->random->next;
+                } else {
+                    originalNode->next->random = NULL;
+                }
+                originalNode = originalNode->next->next;
+            } else {
+                originalNode = originalNode->next;
+            }
+        }
+        // de reference the architecture
+        originalNode = head;
+        cloneNode = clonehead;
+        while(originalNode!=NULL&&cloneNode!=NULL){
+            originalNode->next = cloneNode->next;
+            originalNode = cloneNode->next;
+            if(originalNode!=NULL){
+                cloneNode->next = originalNode->next;
+            }
+            else{
+                cloneNode->next = NULL;
+            }
+            cloneNode = cloneNode->next;
+        }
+        // return the final head;
+        return clonehead;
     }
 };
