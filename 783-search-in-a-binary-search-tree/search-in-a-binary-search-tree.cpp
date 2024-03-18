db1067fd994a9e308bac4cc74a25c1409645509e
Aryan Malik <94473072+Aryan150304@users.noreply.github.com>
2024-03-18 09:58:54 +0530

    Time: 38 ms (9.45%) | Memory: 33.2 MB (6.50%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-       if(root==NULL){
-        return NULL;
-       }
-       if(root->val==val){
-        return root;
-       }
-       TreeNode* lh = searchBST(root->left,val);
-       if(lh) return lh;
-       TreeNode* rh = searchBST(root->right,val);
-       return rh;
+      // as it is a BST 
+    //   so we need to compare the value 
+    if(root==NULL){
+        return  NULL;
+    }
+    if(root->val==val){
+        return root ;
+    }
+    if(root->val>val){
+        TreeNode *lh = searchBST(root->left,val);
+        if(lh )return lh;
+    }
+        return searchBST(root->right,val);
     }
 };
